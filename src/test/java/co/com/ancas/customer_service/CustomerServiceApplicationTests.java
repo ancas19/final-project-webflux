d16c32754f0aa49b030ed4b9202613ed647a384f
afcastro <afcastro@cotrfa.com.co>
2025-04-16 16:56:19 -0500

    Feat: Initial project

---
+++ src/test/java/co/com/ancas/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ancas.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
